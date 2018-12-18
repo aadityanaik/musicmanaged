801d318d230839f060df53d7744230fa95078a45
aadityanaik <aadi.naik@gmail.com>
2018-12-19 00:20:38 +0530

    Used POST for adduser

--- server.js
+++ server.js
@@ -2,18 +2,32 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
+var bodyParser = require('body-parser')
 var db = require('./model/dbmanager')
 
 var dbManager = new db.DBHandler()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/api/listuser', function(req, res) {
     dbManager.getUser(res)
 })
 
+/*
 app.get('/api/adduser/:username/:password', function(req, res) {
     uname = req.params.username
     pword = req.params.password
 
+    dbManager.addUser(res, uname, pword)
+})
+*/
+
+app.post('/api/adduser', function(req, res) {
+    console.log('Getting dat data')
+    console.log('Body is ', req.body)
+    var uname = req.body.username
+    var pword = req.body.password
     dbManager.addUser(res, uname, pword)
 })
 
--- website/demo.js
+++ website/demo.js
@@ -7,3 +7,25 @@ function getResponseOfAPI(endpoint) {
         console.log(JSON.stringify(myJson))
     })
 }
+
+function adduser(username, password) {
+    // TODO: Make the transfer secure over https by adding that one header everyone talks about
+    
+    var url = "http://localhost:5000/api/adduser"
+    
+    var info = {
+        "username": username,
+        "password": password
+    }
+
+    $.post(url, info, function(data) {
+        console.log(data)
+    }, "json")
+}
+
+function adduserfromform() {
+    username = document.getElementById('username').value
+    password = document.getElementById('password').value
+
+    adduser(username, password)
+}
