b0a66bcdda09e311ed8b98fe2e1cf07305e8e324
aadityanaik <aadi.naik@gmail.com>
2019-01-17 23:20:20 +0530

    Automated data dir creation

--- server-test.bat
+++ server-test.bat
@@ -1,2 +1,4 @@
+if not exist "%CD%\data" mkdir "%CD%\data"
+
 START /B "" "D:\MongoDB\Server\4.0\bin\mongod.exe" --dbpath "%CD%\data"
 nodemon server.js
--- server-test.sh
+++ server-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p $PWD/data
+
 mongod --dbpath $PWD/data/ &
 
 nodemon server.js
