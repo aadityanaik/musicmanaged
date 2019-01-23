c7af9d7a8d4bce9d8f051e11685a0ffc56b24bca
Utkarsh Pant <utkarsh.pant@outlook.com>
2019-01-23 17:54:24 +0530

    Added front end views and set templating
    Issues include duplicate user creation.

--- server.js
+++ server.js
@@ -11,9 +11,25 @@ var mongoDBManager = new mongoDB.MongoDBHandler()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(busboy())
 
-app.get('/api/listuser', function(req, res) {
+app.use(express.static('public'));
+app.use(busboy());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views/');
+
+app.get('/', function(req, res) {
+    res.render('/pages/index');
+});
+
+app.get('/signup', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/signin', function(req, res) {
+    res.render('pages/signin');
+});
+
+app.get('/api/listuser', function (req, res) {
     dbManager.getUser(res)
 })
 
@@ -26,35 +42,35 @@ app.get('/api/adduser/:username/:password', function(req, res) {
 })
 */
 
-app.post('/api/adduser', function(req, res) {
+app.post('/api/adduser', function (req, res) {
     var uname = req.body.username
     var pword = req.body.password
     console.log(uname, pword)
     mongoDBManager.addUser(res, uname, pword)
 })
 
-app.post('/api/verifyuser', function(req, res) {
+app.post('/api/verifyuser', function (req, res) {
     var uname = req.body.username
     var pword = req.body.password
     mongoDBManager.checkCredentials(res, uname, pword)
 })
 
-app.post('/api/addmusicfile', function(req, res) {
+app.post('/api/addmusicfile', function (req, res) {
     var fstream;
     req.pipe(req.busboy);
     let formdata = new Map()
-    
+
     var fileName
     var buffer
 
     req.busboy.on('file', function (fieldname, file, filename) {
         var buffArr = []
 
-        file.on('data', function(data) {
+        file.on('data', function (data) {
             buffArr.push(data)
         })
 
-        file.on('end', function() {
+        file.on('end', function () {
             buffer = Buffer.concat(buffArr)
             fileName = filename
         })
@@ -62,29 +78,27 @@ app.post('/api/addmusicfile', function(req, res) {
 
     var username
 
-    req.busboy.on('field', function(fieldname, val) {
-        if(fieldname == 'username') {
+    req.busboy.on('field', function (fieldname, val) {
+        if (fieldname == 'username') {
             username = val
         }
     })
 
-    req.busboy.on('finish', function() {
+    req.busboy.on('finish', function () {
         console.log(buffer.length)
         mongoDBManager.addMusic(res, username, fileName, buffer)
     })
 })
 
-app.post('/api/getmusicfile', function(req, res) {
+app.post('/api/getmusicfile', function (req, res) {
     console.log(req.body)
     // res.attachment('./media/mudmud/audio/The Godfather Theme Song.wav')
     // res.download('./media/mudmud/audio/The Godfather Theme Song.wav')
     mongoDBManager.getMusic(res, req.body.username, req.body.filename, req.body.fileid)
 })
 
-var server = app.listen(5000, function() {
+var server = app.listen(5000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
 })
-
-app.use(express.static('website'))
