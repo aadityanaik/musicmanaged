fe0769d4e90aa1c7587949fc31cd5c867e05c004
aadityanaik <aadi.naik@gmail.com>
2019-03-01 20:27:49 +0530

    Forgot to add this oops...

---
+++ public/scripts/upload.js
@@ -0,0 +1,29 @@
+function uploadFile() {
+    var files = document.upload_file_form.file.files
+    console.log('In the function')
+    console.log(files[0])
+
+    if (files[0].type.lastIndexOf("audio/", 0) == 0) {
+        // var username = document.getElementById('uploadSongUName').value
+        var file = files[0]
+        var host = window.location.hostname
+        var port = window.location.port
+        var url = "http://" + host + ":" + port + "/api/addmusicfile"
+
+        var formdata = new FormData()
+        formdata.append('data', file)
+        // formdata.append('username', username)
+        formdata.append('name', file.name)
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formdata,
+            contentType: false,
+            processData: false
+        }).done(function (data) {
+            console.log(data)
+        })
+    }
+
+}
