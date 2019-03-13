6b22a92c79fc8a8c2a0e17dff195784e2f8471fc
Utkarsh Pant <utkarsh.pant@outlook.com>
2019-03-13 12:12:07 +0530

    Added player source logic
    Added audio source logic.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -5,7 +5,7 @@ list.wrapFocus = true;
 
 // var list_of_files = Array()
 
-var files = Array()
+var files_global = Array()
 
 $(window).on('pageshow', function() {
     updateFiles()
@@ -15,31 +15,31 @@ function updateFiles() {
     $.ajax({url: 'http://localhost:5000/api/getMusicFiles', success: function (data) {
         document.getElementById("list_files").innerHTML = ""
         files = Array()
-        
+        song_array = Array()
         if (data.listFiles) {
             for(var i = 0; i < data.listFiles.length; i++) {
                 var element = document.createTextNode(data.listFiles[i].file_name)
                 element.id = "userfiles"
 
-                var play = document.createElement("audio")
-                play.controls = "controls"
+                // var play = document.createElement("audio")
+                // play.controls = "controls"
                 srcURL = encodeURI("http://localhost:5000/api/getmusicfile?filename=" + data.listFiles[i].file_name +"&fileid=" + data.listFiles[i].file_id)
-                // list_of_files.push({source: srcURL})
-                files.push({name: data.listFiles[i].file_name, id: data.listFiles[i].file_id})
-                play.src = srcURL
+                // // list_of_files.push({source: srcURL})
+                files_global.push({name: data.listFiles[i].file_name, id: data.listFiles[i].file_id, source: srcURL})
+                // play.src = srcURL
 
-                var downloadbtn = document.createElement("a")
-                downloadbtn.innerText = "Download"
-                downloadbtn.setAttribute("class", "btn btn-primary")
-                downloadbtn.id = "downloadBtn"+i
-                downloadbtn.href = encodeURI("http://localhost:5000/api/getmusicfile?filename=" + data.listFiles[i].file_name +"&fileid=" + data.listFiles[i].file_id)
+                // var downloadbtn = document.createElement("a")
+                // downloadbtn.innerText = "Download"
+                // downloadbtn.setAttribute("class", "btn btn-primary")
+                // downloadbtn.id = "downloadBtn"+i
+                // downloadbtn.href = encodeURI("http://localhost:5000/api/getmusicfile?filename=" + data.listFiles[i].file_name +"&fileid=" + data.listFiles[i].file_id)
                 
-                var btn = document.createElement("button")
-                btn.innerText = "Delete"
-                btn.setAttribute("class", "btn btn-primary")
-                btn.id = "deleteBtn"+i
-                btn.onclick = deleteMusic
-                /*
+                // var btn = document.createElement("button")
+                // btn.innerText = "Delete"
+                // btn.setAttribute("class", "btn btn-primary")
+                // btn.id = "deleteBtn"+i
+                // btn.onclick = deleteMusic
+                
                 $('#list_files')
                 .append("<div class=\" row song-title\">"
                     + "<div class=\"col-sm-8\">"
@@ -49,22 +49,47 @@ function updateFiles() {
                         + "<a class='btn btn-primary' id='download_btn" + i + "' href='" + encodeURI("http://localhost:5000/api/getmusicfile?filename=" + data.listFiles[i].file_name +"&fileid=" + data.listFiles[i].file_id) + "'>Download</a>"
                     + "</div>"                
                     + "<div class=\"col-sm-2\">"
-                        + "<button type=\"button\" class='btn btn-danger' id=\"delete_btn" + i + "\">Delete</button>"
+                        + "<button type=\"button\" class='btn btn-danger btn-delete' id=\"delete_btn" + i + "\">Delete</button>"
                     + "</div>"                
                 + "</div>")
-                */
-                document.getElementById("list_files").append(element)
-                document.getElementById("list_files").append(play)
-                document.getElementById("list_files").append(downloadbtn)
-                document.getElementById("list_files").append(btn)
-                document.getElementById("list_files").append(document.createElement("br"))
+                // document.getElementById("list_files").append(element)
+                // document.getElementById("list_files").append(play)
+                // document.getElementById("list_files").append(downloadbtn)
+                // document.getElementById("list_files").append(btn)
+                // document.getElementById("list_files").append(document.createElement("br"))
             }
+
+            $('#player').attr("src", files_global[0].source)
+            //Deletion function
+            $('.btn-delete').click(function() {
+                console.log("fds")
+                pos = this.id.slice(10)
+            
+                var file = files[pos].name
+                var id = files[pos].id
+
+                var host = window.location.hostname
+                var port = window.location.port
+                var url = "http://" + host + ":" + port + "/api/deletemusicfile"
+
+                var info = {
+                    "filename": file,
+                    "fileid": id
+                }
+
+                $.post(url, info, function(data) {
+                    console.log(data)
+                    updateFiles()
+                }, "json")
+            })
         }
         // list_of_files = Object.assign(list_of_files, files)
     }, cache: false, async: true}).then(function(data, err) {
         // TODO: Set audio paths
     })
 }
+
+$('song-row')
 
 function deleteMusic() {
     console.log("fds")
